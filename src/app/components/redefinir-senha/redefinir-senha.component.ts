4fcdc4675320e945237a16bd57d6c3afa0525319
Dan-souza96 <dhany.0803@hmail.com>
2023-09-21 12:26:45 -0300

    atualização geral

--- src/app/components/redefinir-senha/redefinir-senha.component.ts
+++ src/app/components/redefinir-senha/redefinir-senha.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./redefinir-senha.component.scss']
 })
 export class RedefinirSenhaComponent implements OnInit {
+  email: string = ''; // Adicione a propriedade 'email' aqui
   novaSenha: string = '';
   confirmarSenha: string = '';
   mostrarMensagem: boolean = false;
